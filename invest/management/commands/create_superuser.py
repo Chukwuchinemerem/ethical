cf01caa31c2d51ca12266652e99637ae787960e2
chukwu prince <chukwuchinemerem258@gmail.com>
2026-06-28 01:13:18 +0100

    update

--- invest/management/commands/create_superuser.py
+++ invest/management/commands/create_superuser.py
@@ -1,3 +1,4 @@
+import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
@@ -6,12 +7,40 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if not User.objects.filter(is_superuser=True).exists():
-            User.objects.create_superuser(
-                username='Admin2',
-                email='Admin2@profitlynx.com',
-                password='12345678'
-            )
-            self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
-        else:
+
+        admin_username = os.environ.get('SUPERUSER_USERNAME', 'Admin2')
+        admin_email = os.environ.get('SUPERUSER_EMAIL', 'Admin2@profitlynx.com')
+        admin_password = os.environ.get('SUPERUSER_PASSWORD', '12345678')
+        admin_first_name = os.environ.get('SUPERUSER_FIRST_NAME', 'Admin')
+        admin_last_name = os.environ.get('SUPERUSER_LAST_NAME', 'User')
+        admin_phone = os.environ.get('SUPERUSER_PHONE', '0000000000')
+        admin_country = os.environ.get('SUPERUSER_COUNTRY', 'Unknown')
+
+        if User.objects.filter(is_superuser=True).exists():
             self.stdout.write('Superuser already exists.')
+            return
+
+        if User.objects.filter(username=admin_username).exists():
+            user = User.objects.get(username=admin_username)
+            user.email = admin_email
+            user.first_name = admin_first_name
+            user.last_name = admin_last_name
+            user.phone = admin_phone
+            user.country = admin_country
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(admin_password)
+            user.save()
+            self.stdout.write(self.style.SUCCESS('Existing user upgraded to superuser.'))
+            return
+
+        User.objects.create_superuser(
+            username=admin_username,
+            email=admin_email,
+            password=admin_password,
+            first_name=admin_first_name,
+            last_name=admin_last_name,
+            phone=admin_phone,
+            country=admin_country,
+        )
+        self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
